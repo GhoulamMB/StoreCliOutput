a7401dfcc1072b05325ea8c299ced1d0e4aeb815
okbb <okbb@DESKTOP-D5NB746>
2022-11-13 01:59:36 -0800

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,28 @@
+﻿using CliWrap;
+using CliWrap.Buffered;
+using System.Diagnostics;
+
+
+
+//Method 1 : using CLI Wrap lib
+var result = await Cli.Wrap("powershell.exe").
+    WithArguments("-file C:\\Users\\okbb\\Desktop\\test.ps1").
+    ExecuteBufferedAsync();
+
+string outresult = result.StandardOutput.ToString();
+Console.WriteLine("Method 1 : using CLI Wrap lib");
+Console.WriteLine(outresult);
+
+
+//Method 2 : usig the built-in Process class
+
+Process p = new Process();
+ProcessStartInfo startInfo = new ProcessStartInfo();
+startInfo.FileName = "powershell.exe";
+startInfo.Arguments = "-file C:\\Users\\okbb\\Desktop\\test.ps1";
+p.StartInfo = startInfo;
+p.StartInfo.RedirectStandardOutput = true;
+p.Start();
+string output = p.StandardOutput.ReadToEnd();
+Console.WriteLine("Method 2 : usig the built-in Process class");
+Console.WriteLine(output);
